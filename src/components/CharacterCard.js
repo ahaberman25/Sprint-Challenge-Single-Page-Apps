970590e19050a5224f0b98e2cb4089730fb00c8c
Andrew Haberman <a.haberman25@gmail.com>
2020-03-20 19:52:21 -0400

    added axios / characters / characters list

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios"
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  // console.log('character card', props)
+
+  const [showCharacter, setShowCharacter] = useState([])
+
+  useEffect(() => {
+    // console.log('id prop', props)
+    const id = props.match.params.id;
+    // console.log('id', id)
+       axios
+        .get(`https://rickandmortyapi.com/api/character/${id}`)
+        .then(response => {
+          // console.log('response', response)
+          setShowCharacter([response.data])
+        })
+        .catch(error => {
+          console.error(error);
+        });
+
+  },[props]);
+
+  console.log('showcharacter', showCharacter)
+  
+  return (
+    <div>
+      {showCharacter.map(character => {
+        return <div>
+          <h1>{character.name}</h1>
+          <p>
+            <img src={character.image} alt={character.name} /><br />
+            {character.status} <br />
+            {character.species} <br />
+            {character.gender} <br />
+            {character.origin.name} 
+          </p>
+        </div>
+      })}      
+    </div>
+  )
+
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,36 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react"
+import axios from "axios"
+import { Route, Link } from 'react-router-dom';
+import CharacterCard from "./CharacterCard"
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([])
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios.get('https://rickandmortyapi.com/api/character/')
+      .then(response => {
+        console.log('api', response.data)
+        setCharacters(response.data.results) //info holds page/count/next
+      }).catch(error => {
+        console.log('err', error)
+      })
   }, []);
+
+  console.log('charcters', characters)
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <div>
+          <Route path='/characters/:id' component={(props) => <CharacterCard {...props} info={characters} />} />
+          {characters.map(character => {
+            return <div>
+                    <Link to={`/characters/${character.id}`}><p>{character.name}</p></Link>
+                   </div>
+          })}
+      </div>
     </section>
   );
 }
